80510b923318488e1726e93b77959d8fd9284c71
Tarun Belani <tbelani@ufl.edu>
2021-01-05 00:45:06 -0500

    init

--- Miner.fsx
+++ Miner.fsx
@@ -9,71 +9,70 @@ open System.Security.Cryptography
 open System.Text
 
 type BossMessage =
-    | Zeros of int
+    | Work of int * int * string * int
     | Done of string * string
 
 type WorkerMessage =
-    | Work
-
-// Prefix ID
-let id = "tbelani;"
-// Number of zeros
-let k = fsi.CommandLineArgs |> Array.tail |> Array.head |> int
-// Zeros coin prefix
-let zeros = String.replicate k "0"
-// Number of cores
-let cores = Environment.ProcessorCount
-// Coin length
-let n = 16
+    | GenerateCoin of int * int * string
 
 let system: Akka.Actor.ActorSystem =
-    System.create "miner" <| Configuration.defaultConfig ()
+    create "miner"
+    <| Configuration.defaultConfig ()
 
 let hash (coin: string): string =
+    let sha256 = new SHA256Managed ()
     coin
     |> Encoding.ASCII.GetBytes
-    |> (new SHA256Managed ()).ComputeHash
+    |> sha256.ComputeHash
     |> Array.map (sprintf "%02x")
     |> String.concat ""
 
-let isBitcoin (coin : string) : bool =
+let isBitcoin (coin: string) (k: int): bool =
+    let zeros = String.replicate k "0"
     let prefix = coin.[0 .. k-1]
-    String.Equals (prefix, zeros)
+    zeros = prefix
 
-let generateCoinSuffix () =
+let generateCoinSuffix (n: int) (id: string) =
     let chars = "abcdefghijklmnopqrstuvwxyz0123456789"
     let random = Random ()
     let length = n - id.Length
     [| for _ in 1..length -> chars.[ random.Next chars.Length ] |]
     |> string
 
-let worker (i : int) : Akka.Actor.IActorRef =
-    spawn system (sprintf "worker%d" i) <| fun (mailbox : Actor<WorkerMessage>) ->
+let worker (i: int): Akka.Actor.IActorRef =
+    spawn system
+    <| sprintf "worker%d" i
+    <| fun (mailbox: Actor<WorkerMessage>) ->
         let rec loop () =
             actor {
                 let! message = mailbox.Receive ()
                 match message with
-                | _-> ignore ()
+                | _ -> ignore () // mailbox.Sender () <! ...
                 return! loop ()
             }
         loop ()
 
-let boss : Akka.Actor.IActorRef =
-    spawn system "boss" <| fun (mailbox : Actor<BossMessage>) ->
+let boss: Akka.Actor.IActorRef =
+    spawn system "boss"
+    <| fun (mailbox: Actor<BossMessage>) ->
         let rec loop () =
             actor {
                 let! message = mailbox.Receive ()
                 match message with
-                | Zeros k ->
+                | Work (k, n, id, cores) ->
                     List.map worker [ 1 .. cores ]
-                    |> List.iter (fun worker -> worker <! Work)
+                    |> List.iter (fun worker -> worker <! GenerateCoin (k, n, id))
                 | Done (coin, hash) ->
-                    printfn "%s %s" coin hash
+                    printfn "%s  %s" coin hash
                 return! loop ()
             }
         loop ()
 
-printfn "# of cores: %d" cores
-printfn "%s" (hash "adobra;kjsdfk11")
-// boss <! k
-//     ActorRef ! Broadcast(PoisonPill)
+// Number of zeros
+let k = fsi.CommandLineArgs |> Array.tail |> Array.head |> int
+
+// printfn "%s" <| hash "adobra;kjsdfk11"
+printf "\n"
+// boss <! Work (k, 16, "tbelani;", Environment.ProcessorCount)
+printf "\n"
+// ActorRef ! Broadcast(PoisonPill)
